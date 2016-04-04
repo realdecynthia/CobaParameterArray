f79cb0d7f8dec15f2cbfe643c5f110e03d5bb346
Real De Cynthia <giginyareal@gmail.com>
2016-04-05 04:01:57 +0700

    Added files via upload

---
+++ BelajarParameter1.java
@@ -0,0 +1,20 @@
+public class BelajarParameter1{
+
+public static void bintang()
+{
+for(int i=6;i >=1;i–){
+for(int j=i;j >=1;j–){
+System.out.print(“*”);
+}
+System.out.println();
+}
+}
+
+public static void main(String[]args)
+{
+System.out.println(” Segitiga Bintang bernilai 4″);
+System.out.println();
+bintang();
+
+}
+}
